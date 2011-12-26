f103d8a6a3055df5d8cfc69caf0b55f21869ad67
Shingo Omura <everpeace@gmail.com>
2011-12-26 17:35:02 +0900

    commit multi variage linear regression sample.

--- src/main/scala/org/everpeace/scalala/sample/MultiVariateLinearRegressionSample.scala
+++ src/main/scala/org/everpeace/scalala/sample/MultiVariateLinearRegressionSample.scala
@@ -1,15 +1,21 @@
 package org.everpeace.scalala.sample
 
 import scala.io.Source.fromFile
-import scalala.tensor.::
-import scalala.tensor.mutable._
-import scalala.tensor.dense._
-import scalala.library.Library._
-import scalala.library.Plotting._
+import scalala.scalar._;
+import scalala.tensor.::;
+import scalala.tensor.mutable._;
+import scalala.tensor.dense._;
+import scalala.tensor.sparse._;
+import scalala.library.Library._;
+import scalala.library.LinearAlgebra._;
+import scalala.library.Statistics._;
+import scalala.library.Plotting._;
+import scalala.operators.Implicits._;
 
 /**
- * Created by IntelliJ IDEA.
- * User: Shingo Omura <everpeace_at_gmail_dot_com>
+ * Multi-Variate Linear Regression Sample By Scalala.
+ *
+ * Author: Shingo Omura <everpeace_at_gmail_dot_com>
  */
 
 object MultiVariateLinearRegressionSample {
@@ -17,49 +23,49 @@ object MultiVariateLinearRegressionSample {
   def main(args: Array[String]): Unit = run
 
   def run: Unit = {
-    // データの読み込み
+    // loading sample data
     val reg = "([0-9]+)\\,([0-9]+)\\,([0-9]+)*".r
     val data = DenseMatrix(fromFile("MultiVariateLinearRegression.txt").getLines().toList.flatMap(_ match {
       case reg(x1, x2, y) => Seq((x1.toDouble, x2.toDouble, y.toDouble))
       case _ => Seq.empty
     }): _*)
-    println("読み込まれたデータ:\n\t広さ\t#寝室\t値段\n" + data)
+    println("Data Loaded:\n  Area\t#BedRooms\tPrice\n" + data)
 
-    // 学習データの正規化
+    // normalize features
     var X = data(::, 0 to 1)
     val y = data(::, 2)
     val norm = normalizeFeatures(X)
-    print("\n\n平均(広さ, #寝室) =" + norm._2)
-    print("標準偏差(広さ, #寝室) =" + norm._3)
-    // バイアス項を加えて3次元に [1 ; normalized_x1 ; normalized_x2]
+    print("\n\naverage(Area, #BedRooms) =" + norm._2)
+    print("Std. Dev.(Area, #BedRooms) =" + norm._3)
+    // adding bias term ( X => [1 ; normalized_x1 ; normalized_x2] )
     X = DenseMatrix.horzcat(DenseMatrix.ones[Double](X.numRows, 1), norm._1)
-    print("正規化されたデータ(バイアス項が追加されている):\n" + X)
+    print("Normalized Data (added bias term):\n" + X)
 
-    // 学習パラメータ
-    val alpha = 0.1d // 最急降下法へのパラメータ
-    val num_iters = 100 // 最急降下法の回数
+    // learning parameters
+    val alpha = 0.1d
+    val num_iters = 100
     var theta = Vector.zeros[Double](3).asCol
 
-    // 学習
+    // optimization
     println("\n======学習開始======")
     val result = gradientDescent(X, y, theta, alpha, num_iters)
     println("======学習完了======")
 
-    //学習結果表示
+    // display learned result
     plot((1 to num_iters).toArray, result._2.toArray)
     xlabel("number of iterations")
     ylabel("cost")
     title("History of Cost")
-    print("\n学習結果(パラメータ):\t" + theta.asRow)
-    println("学習結果(コスト):\t" + result._2(num_iters - 1))
-    println("\n\nコスト関数履歴のウィンドウを閉じて修了してください。")
+    print("\nLeraned Parameters(Bias,Area,#BedRooms):\t" + theta.asRow)
+    println("Learned Cost:\t" + result._2(num_iters - 1))
+    println("\n\nTo finish this program, close the cost's history window.")
   }
 
 
-  // thetaにおけるコストを算出する. ( cost = (x*theta -y )/2m)
-  // X: X(i,::) == 各フィーチャー行ベクトル
-  //  y: 各データに対するyの値(列ベクトル)
-  // theta: パラメータ(列ベクトル)
+  // calculating cost. (cost = (x*theta -y )/2m)
+  // X: feature row vector is stored in each row
+  // y: y's column vector coresponding to feature row vector.
+  // theta: parameter column vector
   def computeCost(X: Matrix[Double], y: VectorCol[Double], theta: VectorCol[Double]): Double = {
     val diff = X * theta - y
     val m = y.length
@@ -67,13 +73,12 @@ object MultiVariateLinearRegressionSample {
     cost
   }
 
-  // 最急降下法
-  // 各パラメータをコストが減る方向にalpha倍してずらすのを繰り返す
-  // X: X(i,::) == 各フィーチャー行ベクトル
-  //  y: 各データに対するyの値(列ベクトル)
-  // theta: パラメータ(列ベクトル)
-  // alpha: 坂を下るときにどのくらい下るかの係数
-  // num_iters: 繰り返し回数
+  // Gradient Descent
+  // X: feature row vector is stored in each row
+  // y: y's column vector, each value is corresponding to each feature row vector.
+  // theta: parameter column vector
+  // alpha: learning rate
+  // num_iters: number of iterations
   def gradientDescent(X: Matrix[Double], y: VectorCol[Double], theta: VectorCol[Double], alpha: Double, num_iters: Int) = {
     val m = y.length
     val p = X.numCols
@@ -96,16 +101,18 @@ object MultiVariateLinearRegressionSample {
     (theta, J_history)
   }
 
-  // X = [ x_1; x_2; ...]の各フィーチャベクトルを平均0, 標準偏差1に正規化する
+  // normalize the features to average = 0, standard deviation = 1.0
+  // X: feature row vector is stored in each row.  i.e. each row indicates a specific feature.
   def normalizeFeatures(X: Matrix[Double]) = {
+    // calculate μ (average)
     val mu = mean(X, Axis.Vertical)
-    // 各次元でのσを求める
+    // calculate σ (standard deviation)
     val sigma = DenseVector.zeros[Double](X.numCols).asRow
     for (i <- 0 until X.numCols) sigma(i) = sqrt(variance(X(::, i)))
 
-    // 各データに対して (x-μ)/σ を行うことで正規化
+    // for each feature update x to (x-μ)/σ
     for (i <- 0 until X.numRows)
-      X(i, ::) := ((X(i, ::) :- mu) :/ sigma) //ベクトル演算によりすべての次元を同時に正規化
+      X(i, ::) := (X(i, ::) :- mu) :/ sigma
     (X, mu, sigma)
   }
 }
